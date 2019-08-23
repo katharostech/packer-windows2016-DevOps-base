7e7a12bb8c3b6ea70e92f502bb3f9fa236701010
opax7 <opax@katharostech.com>
2019-08-22 22:39:26 -0500

    first commit

---
+++ cookbooks/packer/metadata.rb
@@ -0,0 +1,7 @@
+name 'packer'
+maintainer 'Opax'
+maintainer_email 'opax@katharostech.com'
+license 'Apache-2.0'
+description 'Resources run during packer builds'
+version '0.1.0'
+chef_version '>= 14.0'
